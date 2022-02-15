becbadaacb632b8b5d11430a474654a9017f5cfb
jakob james <jj25589@ms.wasatch.edu>
2022-02-15 14:02:38 -0700

    Initialized the project
    -just started

---
+++ src/Paddle.java
@@ -0,0 +1,17 @@
+import acm.graphics.GRect;
+
+import java.awt.*;
+
+public class Paddle extends GRect {
+
+    public Paddle (double x, double y, double width, double height){
+        super(x, y, width, height);
+        setFilled(true);
+        setFillColor(Color.RED);
+    }
+
+
+
+
+
+}
